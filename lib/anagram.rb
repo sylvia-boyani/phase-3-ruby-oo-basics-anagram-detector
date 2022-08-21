7804eaaab458b81a9185dc749abb87e28d5221fd
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-08-21 23:44:05 +0300

    commit tree 5c7bfeb194802482ba08c3c27379d2630ed1751c

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,31 @@
 # Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  def tokenize(words)
+    possible_matches = words.map { |word| word.split('') }
+    sum = possible_matches.map { |match| match.map { |letter| letter.ord }.sum  }
+    sum
+  end
+
+  def match(words)
+    sums = tokenize(words)
+    sum = tokenize(@word.split('')).sum
+    index = []
+    sums.each.with_index do |n, i|
+      if n == sum
+        index << i
+      end
+    end
+
+    if index.empty?
+      []
+    elsif index.size == 1
+      [words[*index]]
+    else
+      index.map {|i| words[i]}
+    end
+  end
+end
